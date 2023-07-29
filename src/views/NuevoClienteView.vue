c370b2d8c31487d70e180a0b78df2118d9d7933d
BrandonLozada <brandon_lozada@outlook.com>
2023-07-28 23:38:57 -0600

    Instalar tema genesis de Formkit; Agregar formulario del componente formkit.

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import { FormKit } from '@formkit/vue'
 import RouterLink from '../components/UI/RouterLink.vue'
 import Heading from '../components/UI/Heading.vue'
 
@@ -21,5 +21,30 @@ defineProps({
             </RouterLink>
         </div>
         <Heading class="text-4xl font-extrabold text-slate-500">{{ titulo }}</Heading>
+
+        <div class="mx-auto mt-10 bg-white shadow">
+            <div class="mx-auto md:w-2/3 py-20 px-6">
+                <FormKit
+                    type="form"
+                >
+
+                <FormKit
+                    type="text"
+                    label="Nombre"
+                    placeholder="Nombre de Cliente"
+                    help="Coloca el Nombre del Cliente que deseas registrar"
+                    validation="required"
+                    :validation-messages="{ required: 'El Nombre del Cliente es obligatorio' }"
+                />
+
+                </FormKit>
+            </div>
+        </div>
     </div>
 </template>
+
+<style>
+    .formkit-wrapper {
+        max-width: 100%;
+    }
+</style>
