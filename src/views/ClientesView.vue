be43eb84a9d6a7d00b040e250a5ea73a31a9c8f1
BrandonLozada <brandon_lozada@outlook.com>
2023-07-29 18:02:31 -0600

    Consumir endpoint de obtener clientes

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,15 @@
 <script setup>
+import { onUnmounted } from 'vue'
+import axios from 'axios'
 import RouterLink from '../components/UI/RouterLink.vue'
 import Heading from '../components/UI/Heading.vue'
+
+onUnmounted(()=> {
+    axios.get('http://localhost:4000/clientes')
+        .then(respuesta => {
+            console.log("respuesta", respuesta)
+        })
+})
 
 defineProps({
     titulo: {
